cebb7a2a8c3a5b603a10737c3b522bf891ab9996
Matheus Dias <matheusmelhor3@outlook.com>
2023-01-28 15:17:36 -0300

    foi

---
+++ src/app/dnd.directive.ts
@@ -0,0 +1,41 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDnd]'
+})
+export class DndDirective {
+
+  @HostBinding('class.fileover') fileOver: boolean = false;
+
+  constructor() { }
+
+  //Dragover listener
+  @HostListener('dragover', ['$event']) onDragOver(evt: any) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    console.log('Drag over');
+  }
+
+  //Dragleave listener
+  @HostListener('dragleave', ['$event']) onDragLeave(evt: any) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    console.log('Drag leave');
+  }
+
+  //Drop listener
+  @HostListener('drop', ['$event']) onDrop(evt: any) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.fileOver = false;
+    const files = evt.dataTransfer.files;
+    if (files.length > 0)
+    {
+      this.fileDropped.emmit(files);
+      console.log(`voce botou ${files.length} arquivos`);
+    }
+
+  }
+
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  providers: [NgbModalConfig, NgbModal],
+})
+export class HomeComponent {
+
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+		// customize default values of modals used by this component tree
+		config.backdrop = 'static';
+		config.keyboard = false;
+	}
+	open(content: any) {
+		this.modalService.open(content);
+	}
+}
